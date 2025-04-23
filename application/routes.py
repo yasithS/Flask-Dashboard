33935b41560e03c13335fe62eb49d2af4a21c840
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-04-23 12:38:07 +0530

    feat: dynamic title

--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 @app.route("/")
 def indeex():
-    return render_template("index.html")
+    return render_template("index.html", title="Home Page")
 
 @app.route("/base")
 def layout():
