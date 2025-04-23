243b43f08b4ac296a48cb69f20704ee1527563a9
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-04-23 12:26:39 +0530

    chore: template inheritance

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,10 @@
 from application import app
+from flask import render_template
 
 @app.route("/")
 def indeex():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route("/base")
+def layout():
+    return render_template("base.html")
